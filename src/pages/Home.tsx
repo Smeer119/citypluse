6545e0bbdfcdf6426ebd260038af9080ad699e29
tejashwini-Bk-tech <tejashwinikoli2005@gmail.com>
2025-08-17 17:12:19 +0530

    signup and singin

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -73,18 +73,16 @@ const Home = () => {
                     For citizens and city administrators.
                   </CardDescription>
                   <div className="space-y-3">
-                    <Button size="lg" variant="outline" className="w-full border-primary text-primary hover:bg-primary/10">
-                    
+                    <Button size="lg" variant="outline" className="w-full border-primary text-primary hover:bg-primary/10" asChild>
                       <Link to="/SignUp">
-                       <UserPlus className="w-5 h-5 mr-2">
-                      Sign Up
-                    </Link>
-                     
+                        <UserPlus className="w-5 h-5 mr-2" />
+                        Sign Up
+                      </Link>
                     </Button>
-                    <Button size="lg" variant="ghost" className="w-full hover:bg-secondary">
-                      <Link to="/SignIp">
-                      <LogIn className="w-5 h-5 mr-2" />
-                      Sign In
+                    <Button size="lg" variant="ghost" className="w-full hover:bg-secondary" asChild>
+                      <Link to="/SignIn">
+                        <LogIn className="w-5 h-5 mr-2" />
+                        Sign In
                       </Link>
                     </Button>
                   </div>
